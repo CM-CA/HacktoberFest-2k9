eb6fb40283e986a7c36482659736fb9da80240c4
Carlacid <carlacid@localhost.localdomain>
2019-10-08 16:09:44 +0200

    change format

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,52 @@
 <template>
   <div id="app">
     <div id="navbar-component">
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand href="#">Icono</b-navbar-brand>
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#">Icono</b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item active href="#">CARRETERA</b-nav-item>
-          <b-nav-item active href="#">MONTAÑA</b-nav-item>
-        </b-navbar-nav>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item active href="#">CARRETERA</b-nav-item>
+            <b-nav-item active href="#">MONTAÑA</b-nav-item>
+          </b-navbar-nav>
 
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
 
-          <b-nav-item-dropdown right>
-            <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
-  <div>
-    <b-carousel
+            <b-nav-item-dropdown right>
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <div>
+      <b-carousel
       id="carousel-fade"
       style="text-shadow: 0px 0px 2px #000"
       fade
       indicators
       img-width="1024"
-      img-height="300"
-    >
-      <b-carousel-slide
+      img-height="300">
+        <b-carousel-slide
         caption="First slide"
-        img-src="https://picsum.photos/1024/480/?image=10"
-      ></b-carousel-slide>
-      <b-carousel-slide
+        img-src="https://picsum.photos/1024/480/?image=10"></b-carousel-slide>
+        <b-carousel-slide
         caption="Second Slide"
-        img-src="https://picsum.photos/1024/480/?image=12"
-      ></b-carousel-slide>
-      <b-carousel-slide
+        img-src="https://picsum.photos/1024/480/?image=12"></b-carousel-slide>
+        <b-carousel-slide
         caption="Third Slide"
-        img-src="https://picsum.photos/1024/480/?image=22"
-      ></b-carousel-slide>
-    </b-carousel>
+        img-src="https://picsum.photos/1024/480/?image=22"></b-carousel-slide>
+      </b-carousel>
     </div>
 
   </div>
