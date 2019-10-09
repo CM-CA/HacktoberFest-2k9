410991866956c45257a9532467a7c9e6ad8e8c5c
Carlacid <carlacidf98@gmail.com>
2019-10-09 13:53:42 +0200

    add component remove coment

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -29,25 +29,7 @@
         </b-collapse>
       </b-navbar>
     </div>
-    <div>
-      <b-carousel
-      id="carousel-fade"
-      style="text-shadow: 0px 0px 2px #000"
-      fade
-      indicators
-      img-width="1024"
-      img-height="300">
-        <b-carousel-slide
-        caption="First slide"
-        img-src="https://picsum.photos/1024/480/?image=10"></b-carousel-slide>
-        <b-carousel-slide
-        caption="Second Slide"
-        img-src="https://picsum.photos/1024/480/?image=12"></b-carousel-slide>
-        <b-carousel-slide
-        caption="Third Slide"
-        img-src="https://picsum.photos/1024/480/?image=22"></b-carousel-slide>
-      </b-carousel>
-    </div>
+    <carousel></carousel>
 
   </div>
 </template>
@@ -57,6 +39,6 @@ export default {
   name:"App"
 }
 </script>
-<style lang="scss">
+<style>
 
 </style>
