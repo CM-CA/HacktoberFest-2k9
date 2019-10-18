34b5085391d717527e0d67d481fec1e57d6e7d5e
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2019-10-18 18:53:20 +0200

    fake api (#38)
    * fake api
    
    * Update Mountainview.vue
    
    Delete commented code.

--- frontend/src/views/Mountainview.vue
+++ frontend/src/views/Mountainview.vue
@@ -14,14 +14,77 @@
         <p>A continuacion te mostramos nuestra mejor seleccion en grandes marcas.</p>
       </b-jumbotron>
     </div>
-    <div id="mountain-body">
-      <!--Not defined -->
+    <div id="mountain-body" class="container-fluid">
+      <div class="card" style="width: 18rem;" v-for="b in bikes">
+        <img :src="b.img" class="card-img-top" :alt="b.model" />
+        <div class="card-body">
+          <h5 class="card-title">{{b.model}}</h5>
+          <p class="card-text">{{b.brand}}, tamaño de rueda: {{b.size}} pulgadas.</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  name: "mountain-in-view",
+
+  data() {
+    return {
+      //  fake API
+      bikes: [
+        {
+          id: "1",
+          brand: "Ibis",
+          model: "Ripmo GX 2019",
+          size: "29",
+          img:
+            "https://outdoorgearlab-mvnab3pwrvp3t0.stackpathdns.com/photos/17/62/297773_8160_M.jpg"
+        },
+        {
+          id: "2",
+          brand: "Yeti",
+          model: "SB130 TURQ X01 2019",
+          size: "29",
+          img:
+            "https://outdoorgearlab-mvnab3pwrvp3t0.stackpathdns.com/photos/18/54/306942_11028_M.jpg"
+        },
+        {
+          id: "3",
+          brand: "Ibis",
+          model: "Ripley GX Eagle 2019",
+          size: "29",
+          img:
+            "https://outdoorgearlab-mvnab3pwrvp3t0.stackpathdns.com/photos/19/77/319226_7308_M.jpg"
+        },
+        {
+          id: "4",
+          brand: "Santa Cruz",
+          model: "Hightower CC X01",
+          size: "29",
+          img:
+            "https://outdoorgearlab-mvnab3pwrvp3t0.stackpathdns.com/photos/20/42/325773_14076_M.jpg"
+        },
+        {
+          id: "5",
+          brand: "YT",
+          model: "Jaffsy AL Base 2019",
+          size: "29",
+          img:
+            "https://outdoorgearlab-mvnab3pwrvp3t0.stackpathdns.com/photos/19/30/314571_25718_M.jpg"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style scoped>
+<style>
+#mountain-body{
+display:flex;
+
+}
+.card {
+  margin:0.2rem;
+}
 </style>>
